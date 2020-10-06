3dc79c9a68e0f64f80cc394fa5ec004acb7a25d4
Ana <ana.barbu.19@gmail.com>
2020-10-06 08:45:18 -0400

    Update video.js

--- video.js
+++ video.js
@@ -2,7 +2,7 @@ $(function(){
   var $refreshButton = $('#refresh');
   var $results = $('#css_result');
   videojs("my_video_1", { "controls": true, "autoplay": true, "preload": "auto" });
-var player = videojs('vid1');
+var player = videojs('my_video_1');
 
 player.breakpoints(true);
 });
