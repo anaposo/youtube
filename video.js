cdfa8efa6e6645319868d46ed3632f1b05619665
Ana <ana.barbu.19@gmail.com>
2020-10-06 07:20:05 -0400

    Add files via upload

---
+++ video.js
@@ -0,0 +1,23 @@
+$(function(){
+  var $refreshButton = $('#refresh');
+  var $results = $('#css_result');
+  videojs("my_video_1", { "controls": true, "autoplay": true, "preload": "auto" });
+var player = videojs('vid1');
+
+player.breakpoints(true);
+});
+  
+  function refresh(){
+    var css = $('style.cp-pen-styles').text();
+    $results.html(css);
+  }
+
+  refresh();
+  $refreshButton.click(refresh);
+  
+  // Select all the contents when clicked
+  $results.click(function(){
+    $(this).select();
+  });
+});
+
